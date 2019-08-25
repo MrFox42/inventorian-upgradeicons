5ed35db91e5736e58d6dc8760f1b2a67b41ed9db
MrFox <gyussz@live.com>
2019-08-25 15:11:11 +0200

    Replace tabs with spaces

--- Inventorian-UpgradeIcons.lua
+++ Inventorian-UpgradeIcons.lua
@@ -2,20 +2,20 @@ local Addon = LibStub("AceAddon-3.0"):GetAddon("Inventorian")
 local Update = Addon.Item.prototype.Update
 
 Addon.Item.prototype.IsUpgrade = function(self, ...)
-	return IsContainerItemAnUpgrade(self:GetBag(), self:GetID())
+    return IsContainerItemAnUpgrade(self:GetBag(), self:GetID())
 end
 
 Addon.Item.prototype.UpdateUpgradeIcon = function(self, ...)
-	local isUpgrade = self:IsUpgrade()
-	if isUpgrade == nil then
-		self:After(0.5, 'UpdateUpgradeIcon')
-	else
-		self.UpgradeIcon:SetShown(isUpgrade)
-	end
+    local isUpgrade = self:IsUpgrade()
+    if isUpgrade == nil then
+        self:After(0.5, 'UpdateUpgradeIcon')
+    else
+        self.UpgradeIcon:SetShown(isUpgrade)
+    end
 end
 
 -- Override the current Update function to call UpdateUpgradeIcon
 Addon.Item.prototype.Update = function(self, ...)
     Update(self, ...)
-	self:UpdateUpgradeIcon()
+    self:UpdateUpgradeIcon()
 end
